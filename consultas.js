6345cc8c6c3d89b09aac23ed092efcf2e815cb91
Luis Lizama <l.lizamamejias@hotmail.com>
2021-10-14 13:17:46 -0300

    equipo 2 listo

--- consultas.js
+++ consultas.js
@@ -1,22 +1,87 @@
-const { Pool } = require('pg')
+const {
+    Pool
+} = require('pg')
 
 const pool = new Pool({
     user: 'postgres',
-    password: 'postgres',
-    database: 'pgnode',
     host: 'localhost',
-    port: 5432
+    database: 'postgres',
+    password: '123123123',
+    server: "localhost",
+    port: 5432,
 })
 
-let listado = async() => {
-    try {
-        let resultado = await pool.query("SELECT * FROM bootcamps")
-        return resultado    
-    } catch (error) {
-        console.log(error.code)
-        return error
-    }
+const agregar = async (datos) => {
+    pool.connect(async (e, cliente, release) => {
+        const consulta = {
+            text: `INSERT INTO bootcamps(nombre, duracion, turno)
+        VALUES($1, $2, $3) RETURNING *`,
+            values: datos
+        }
+
+        try {
+            const resultado = await cliente.query(consulta)
+            return resultado
+        } catch (error) {
+            console.log(error)
+        }
+        release()
+    })
 }
 
-module.exports = { listado }
 
+const obtener = () => {
+    return new Promise((res, req) => { 
+        pool.connect(async (e, cliente, release) => {
+        const resultado = await cliente.query("SELECT * FROM bootcamps")
+        release()
+        res(resultado);
+       
+    })})
+   
+    
+
+}
+
+const editar = (datos) => {
+    pool.connect(async (e, cliente, release) => {
+
+        const consulta = {
+            text: `UPDATE bootcamps SET nombre = $1,  duracion = $2, turno = $3 WHERE id=$4 RETURNING *;`,
+            values: Object.values(datos)
+        }
+        try {
+            const resultado = await cliente.query(consulta)
+            console.log("registro Actualizado")
+            return resultado
+        } catch (error) {
+            console.log(error)
+        }
+        release()
+
+    })
+}
+
+const borrar = async (id) => {
+    pool.connect(async (e, cliente, release) => {
+        let consulta = {
+            text: `DELETE FROM bootcamps WHERE id = $1 RETURNING *`,
+            values: [id]
+        }
+        try {
+            const resultado = await cliente.query(consulta)
+            return resultado
+        } catch (error) {
+            console.log(error)
+        }
+        release()
+
+    })
+
+}
+module.exports = {
+    agregar,
+    obtener,
+    editar,
+    borrar
+}
--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const http = require('http')
-const { listado } = require('./consultas')
+const fs = require('fs')
+const url = require('url')
+
+const { agregar,
+    obtener,
+    editar,
+    borrar } = require('./consultas')
 
 http.createServer(async (req, res) =>{
-
-    if(req.url.startsWith('/bootcamps') && req.method == 'GET'){
-        res.setHeader('content-type','application/json')
-        const resultado = await listado()
-        res.end(JSON.stringify(resultado.rows))
+    if (req.url == "/") {
+        const html = fs.readFileSync('./public/index.html', 'utf-8')
+        res.setHeader('content-type', 'text/html')
+        res.end(html)
     }
+
+    if (req.url.startsWith("/turno") && req.method == "POST") {
+        let body = ""
+        req.on("data", (datos) => {
+            body += datos
+        })
+        req.on("end", async () => {
+            // console.log(JSON.parse(body))
+            await agregar(Object.values(JSON.parse(body)))
+            res.end()
+        })
+    }
+    if (req.url.startsWith("/turno") && req.method == 'GET') {
+        const turno = await obtener()
+        res.setHeader('content-type', 'application/json')
+        // console.log(turno.rows)
+        res.end(JSON.stringify(turno.rows))
+       
+    }
+
+    if (req.url.startsWith("/turno") && req.method == 'PUT') {
+        let body = ""
+        req.on("data", (datos) => {
+            body += datos
+        })
+        req.on("end",async () => {
+            const datosEditar = Object.values(JSON.parse(body))
+            console.log(datosEditar)
+            const respuesta = await editar(datosEditar)
+            res.end(JSON.stringify(respuesta))
+        })
+    }
+    if (req.url.startsWith("/turno") && req.method == 'DELETE') {
+        const id = url.parse(req.url, true).query.id
+        await borrar(id)
+        console.log(id)
+        res.end()
+    }
+
 
 }).listen(3000, () => console.log("Ejecutando http://localhost:3000"))
